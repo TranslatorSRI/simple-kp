bf69cdbc1baec977c9c86741f44961b5a176250f
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-01 10:53:48 -0500

    Add/improve tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,6 +14,18 @@ setup_logger()
 
 
 @pytest.fixture
+async def connection():
+    """Return FastAPI app fixture."""
+    async with aiosqlite.connect(":memory:") as connection:
+        await add_data(
+            connection,
+            nodes_file=mychem.nodes_file,
+            edges_file=mychem.edges_file,
+        )
+        yield connection
+
+
+@pytest.fixture
 async def kp():
     """Return FastAPI app fixture."""
     async with aiosqlite.connect(":memory:") as connection:
@@ -26,7 +38,7 @@ async def kp():
 
 
 @pytest.mark.asyncio
-async def test_main(kp: KnowledgeProvider):
+async def test_reverse(kp: KnowledgeProvider):
     """Test simple KP."""
     message = {
         "query_graph": {
@@ -56,6 +68,75 @@ async def test_main(kp: KnowledgeProvider):
     kgraph, results = await kp.get_results(message["query_graph"])
     assert results
     print(json.dumps(results, indent=4))
+
+
+@pytest.mark.asyncio
+async def test_forward(kp: KnowledgeProvider):
+    """Test subject->object lookup."""
+    message = {
+        "query_graph": {
+            "nodes": {
+                "n0": {
+                    "category": "biolink:Disease",
+                },
+                "n1": {
+                    "category": "biolink:ChemicalSubstance",
+                    "id": "CHEBI:136043",
+                },
+            },
+            "edges": {
+                "e01": {
+                    "subject": "n1",
+                    "object": "n0",
+                    "predicate": "biolink:treats",
+                },
+            },
+        },
+        "results": {},
+        "knowledge_graph": {
+            "nodes": {},
+            "edges": {},
+        },
+    }
+    kgraph, results = await kp.get_results(message["query_graph"])
+    assert results
+    print(json.dumps(results, indent=4))
+
+
+@pytest.mark.asyncio
+async def test_no_reverse(connection: aiosqlite.Connection):
+    """Test prohibited object->subject lookup."""
+    kp = KnowledgeProvider(
+        connection,
+        object_to_subject=False,
+    )
+    message = {
+        "query_graph": {
+            "nodes": {
+                "n0": {
+                    "category": "biolink:Disease",
+                    "id": "MONDO:0005148",
+                },
+                "n1": {
+                    "category": "biolink:ChemicalSubstance",
+                },
+            },
+            "edges": {
+                "e01": {
+                    "subject": "n1",
+                    "object": "n0",
+                    "predicate": "biolink:treats",
+                },
+            },
+        },
+        "results": {},
+        "knowledge_graph": {
+            "nodes": {},
+            "edges": {},
+        },
+    }
+    kgraph, results = await kp.get_results(message["query_graph"])
+    assert results == []
 
 
 @pytest.mark.asyncio
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,11 +1,64 @@
 """Test operations."""
-from fastapi.testclient import TestClient
-from simple_kp.server import app
+import pytest
 
-client = TestClient(app)
+import aiosqlite
+
+from simple_kp.build_db import add_data
+from simple_kp.engine import KnowledgeProvider
+from small_kg import mychem
+
+from tests.logging_setup import setup_logger
+
+setup_logger()
 
 
-def test_ops():
-    """Test getting operations."""
-    response = client.get('/FOTR/ops')
-    print(response.json())
+@pytest.fixture
+async def connection():
+    """Return FastAPI app fixture."""
+    async with aiosqlite.connect(":memory:") as connection:
+        await add_data(
+            connection,
+            nodes_file=mychem.nodes_file,
+            edges_file=mychem.edges_file,
+        )
+        yield connection
+
+
+@pytest.mark.asyncio
+async def test_ops(connection: aiosqlite.Connection):
+    """Test KP operations."""
+    kp = KnowledgeProvider(connection)
+    ops = await kp.get_operations()
+    assert len(ops) == 2
+
+    kp = KnowledgeProvider(
+        connection,
+        subject_to_object=False,
+    )
+    ops = await kp.get_operations()
+    assert len(ops) == 1
+    assert ops[0] == {
+        "source_type": "biolink:Disease",
+        "edge_type": "<-biolink:treats-",
+        "target_type": "biolink:ChemicalSubstance",
+    }
+
+    kp = KnowledgeProvider(
+        connection,
+        object_to_subject=False,
+    )
+    ops = await kp.get_operations()
+    assert len(ops) == 1
+    assert ops[0] == {
+        "source_type": "biolink:ChemicalSubstance",
+        "edge_type": "-biolink:treats->",
+        "target_type": "biolink:Disease",
+    }
+
+    kp = KnowledgeProvider(
+        connection,
+        object_to_subject=False,
+        subject_to_object=False,
+    )
+    ops = await kp.get_operations()
+    assert len(ops) == 0
